8b0ed1fa66ab89c49fd68044930048387631d973
htsign <htsign@outlook.com>
2017-02-11 23:59:36 +0900

    コメントつけました
    あとRegisterProviderのシグネチャに破壊的変更

--- Net/LyricsFetcher.cs
+++ Net/LyricsFetcher.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Xml.Linq;
@@ -13,30 +14,62 @@ namespace MusicBeePlugin.Net
         #endregion
 
         #region public properties
+        /// <summary>
+        /// LyricsFetcher に登録済の歌詞提供事業者名リスト
+        /// </summary>
         public static string[] RegisteredProviders => registeredProviders.Keys.ToArray();
 
+        /// <summary>
+        /// 一意に識別可能な歌詞提供事業者名
+        /// </summary>
         public abstract string Provider { get; }
         #endregion
 
         static LyricsFetcher()
         {
-            // RegisterProvider(new InheritLyricsFetcher());
+            // RegisterProvider<JLyricLyricsFetcher>();
         }
 
         #region public methods
-        public static bool RegisterProvider(string provider, LyricsFetcher fetcher)
+        /// <summary>
+        /// 歌詞取得クラスを登録します。
+        /// </summary>
+        /// <typeparam name="TLyricsFetcher"></typeparam>
+        /// <returns></returns>
+        public static bool RegisterProvider<TLyricsFetcher>()
+            where TLyricsFetcher : LyricsFetcher, new()
         {
+            var fetcher = new TLyricsFetcher();
+            string provider = fetcher.Provider;
+
             if (registeredProviders.ContainsKey(provider)) return false;
             registeredProviders[provider] = fetcher;
             return true;
         }
 
+        /// <summary>
+        /// 登録済みの歌詞取得クラスを取得します。
+        /// </summary>
+        /// <param name="providerName"></param>
+        /// <returns></returns>
         public static LyricsFetcher GetFetcher(string providerName) => registeredProviders[providerName];
 
+        /// <summary>
+        /// タグを元に歌詞を取得します。
+        /// </summary>
+        /// <param name="title">タイトル</param>
+        /// <param name="artist">アーティスト</param>
+        /// <param name="album">アルバム</param>
+        /// <returns></returns>
         public abstract string Fetch(string title, string artist, string album = null);
         #endregion
 
         #region private methods
+        /// <summary>
+        /// リクエスト先のHTMLをXMLドキュメントとして非同期に取得します。
+        /// </summary>
+        /// <param name="url">リクエストURL</param>
+        /// <returns></returns>
         protected Task<XDocument> DownloadPageAsXmlAsync(string url) =>
             Task.Factory.StartNew(() => new SgmlReader { Href = url, IgnoreDtd = true })
             .ContinueWith(t => XDocument.Load(t.Result));
