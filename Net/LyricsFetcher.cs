7d4ecb04ae69061d519f5cebbe3b7f08cd59422b
htsign <htsign@outlook.com>
2017-02-12 01:41:32 +0900

    どうせMusicBee側でプラグインは非同期に呼ばれるし、非同期メソッドをバッサリ削除しました

--- Net/LyricsFetcher.cs
+++ Net/LyricsFetcher.cs
@@ -1,7 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Net;
+using System.Text;
 using System.Xml.Linq;
 using Sgml;
 
@@ -66,13 +67,60 @@ namespace MusicBeePlugin.Net
 
         #region private methods
         /// <summary>
-        /// リクエスト先のHTMLをXMLドキュメントとして非同期に取得します。
+        /// リクエスト先のHTMLをXMLドキュメントとして取得します。
+        /// </summary>
+        /// <param name="url">リクエストURL</param>
+        /// <param name="encoding">取得対象ドキュメントのエンコーディング</param>
+        /// <param name="headers">追加HTTPヘッダ</param>
+        /// <returns></returns>
+        protected XDocument DownloadPageAsXml(
+            string url,
+            Encoding encoding,
+            IDictionary<HttpRequestHeader, string> headers)
+        {
+            var wc = new WebClientEx { Encoding = encoding };
+            if (headers != null)
+            {
+                foreach (KeyValuePair<HttpRequestHeader, string> header in headers)
+                {
+                    wc.Headers[header.Key] = header.Value;
+                }
+            }
+            string html = wc.DownloadString(url);
+            
+            using (var reader = new SgmlReader { DocType = "HTML", IgnoreDtd = true })
+            using (var ms = new MemoryStream(Encoding.UTF8.GetBytes(html)))
+            using (var sr = new StreamReader(ms))
+            {
+                reader.InputStream = sr;
+                return XDocument.Load(reader);
+            }
+        }
+
+        /// <summary>
+        /// リクエスト先のHTMLをXMLドキュメントとして取得します。
+        /// </summary>
+        /// <param name="url">リクエストURL</param>
+        /// <param name="encoding">取得対象ドキュメントのエンコーディング</param>
+        /// <returns></returns>
+        protected XDocument DownloadPageAsXml(string url, Encoding encoding)
+            => DownloadPageAsXml(url, encoding, null);
+
+        /// <summary>
+        /// リクエスト先のHTMLをXMLドキュメントとして取得します。
+        /// </summary>
+        /// <param name="url">リクエストURL</param>
+        /// <param name="headers">追加HTTPヘッダ</param>
+        /// <returns></returns>
+        protected XDocument DownloadPageAsXml(string url, IDictionary<HttpRequestHeader, string> headers)
+            => DownloadPageAsXml(url, Encoding.UTF8, headers);
+
+        /// <summary>
+        /// リクエスト先のHTMLをXMLドキュメントとして取得します。
         /// </summary>
         /// <param name="url">リクエストURL</param>
         /// <returns></returns>
-        protected Task<XDocument> DownloadPageAsXmlAsync(string url) =>
-            Task.Factory.StartNew(() => new SgmlReader { Href = url, IgnoreDtd = true })
-            .ContinueWith(t => XDocument.Load(t.Result));
+        protected XDocument DownloadPageAsXml(string url) => DownloadPageAsXml(url, Encoding.UTF8);
         #endregion
     }
 }
